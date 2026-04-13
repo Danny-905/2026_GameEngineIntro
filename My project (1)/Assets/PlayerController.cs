9abeb5be1de5907e6621c3fa19b47a40f8747d17
단니 <qweruiop0905@gmail.com>
2026-04-13 21:01:50 +0900

    update
    캐릭터, 움직임, 맵

---
+++ My project (1)/Assets/PlayerController.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Vector2 moveInput;
+    public float moveSpeed = 7f;
+    public float jumpForce = 7f;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    public void OnJump(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
+
+    void Update()
+    {
+        if(moveInput.x > 0)
+        {
+            transform.localScale = new Vector3(1,1,1);
+        }
+        else if(moveInput.x < 0)
+        {
+            transform.localScale = new Vector3(-1,1,1);
+        }
+
+        transform.Translate(Vector3.right * moveSpeed * moveInput.x * Time.deltaTime);
+    }
+}
